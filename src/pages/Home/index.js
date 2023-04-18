f2854c7c501fe74834c9184dcc8a165a332b01a3
Franklin Angelo <angelod1@hotmail.com>
2023-04-18 16:13:44 -0300

    secao de profile

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,16 @@ const App = ()=> {
             <input className="input" name="usuario" placeholder="@username"/>
             <button className="button">Buscar</button>
           </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/22668960?v=4"
+             className="profile" alt="imagem de perfil"/>
+            <div>
+              <h3>Franklin</h3>
+              <span>@arenas-franklin</span>
+              <p>Descrição </p>
+            </div>
+          </div>
+          <hr/>
         </div>
         </div>
       </div>
