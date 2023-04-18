2bdf4cdf1b87a24d6d2a07dc418f4953e1c4c33a
Franklin Angelo <angelod1@hotmail.com>
2023-04-18 15:45:42 -0300

    criando o input e button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Header } from "../../components/Header";
+import background from "../../assets/background.png"
+import "./styles.css"
 
 const App = ()=> {
     return (
       <div className="App">
-        <Header></Header>
+        <Header/>
+        <div className="conteudo">
+        <img src={background} className="background" alt="background app"/>
+        <div className="info">
+          <div>
+            <input className="input" name="usuario" placeholder="@username"/>
+            <button className="button">Buscar</button>
+          </div>
+        </div>
+        </div>
       </div>
     );
   }
